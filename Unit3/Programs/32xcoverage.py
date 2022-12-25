0360ffd10d07d8ce015a18a5bd0b18aafa028577
iankorf <ifkorf@gmail.com>
2022-12-24 23:53:39 -0800

    init

---
+++ Unit3/Programs/32xcoverage.py
@@ -0,0 +1,18 @@
+# 32xcoverage.py
+
+# Write a program that simulates a shotgun resequencing project
+# Generate reads randomly over a genome
+# How uniformly do the reads "pile up" on a chromosome?
+# How much of that depends on sequencing depth?
+
+# Report min, max, and average coverage
+# Make variables for genome size, read number, read length
+# Input values from the command line
+
+import sys
+import random
+
+"""
+python3 32xcoverage.py 1000 100 100
+5 20 10.82375
+"""
