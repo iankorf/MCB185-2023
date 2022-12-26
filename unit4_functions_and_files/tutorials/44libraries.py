182d9e6428d4cf83039f702d7d200029b654bd7e
iankorf <ifkorf@gmail.com>
2022-12-25 23:45:51 -0800

    up

---
+++ unit4_functions_and_files/tutorials/44libraries.py
@@ -0,0 +1,13 @@
+# 44libraries.py
+
+# Move the triple quotes downward to uncover each segment of code
+
+# You have used various libraries before (math, sys, gzip)
+
+"""
+
+goals
+	create and use a library
+
+
+"""
